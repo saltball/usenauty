0afd7359f8ea471c73340b783bc9e7dca8c30655
saltball <yanbohan98@gmail.com>
2021-06-30 03:15:49 +0800

    light version of output done.

--- include/cagesearch.h
+++ include/cagesearch.h
@@ -6,184 +6,31 @@
 #include <vector>
 #include <string>
 
-typedef int _Boolean;
-typedef unsigned long setword;
-typedef setword set;
-
-#define BIAS6 63
-#define MAXBYTE 126
-#define SMALLN 62
-#define SMALLISHN 258047
-#define TOPBIT6 32
-#define C6MASK 63
-
-#define FALSE 0
-#define TRUE 1
+#include "nauty.h"
+#include "gtools.h"
+#include "naugroup.h"
 
 #define B(i) (1 << ((i)-1))
 #define M(i) ((1 << (i)) - 1)
 
-#define SIZELEN(n) ((n) <= SMALLN ? 1 : ((n) <= SMALLISHN ? 4 : 8))
-/* length of size code in bytes */
-#define G6BODYLEN(n) \
-    (((size_t)(n) / 12) * ((size_t)(n)-1) + (((size_t)(n) % 12) * ((size_t)(n)-1) + 11) / 12)
-#define G6LEN(n) (SIZELEN(n) + G6BODYLEN(n))
-/* exact graph6 string length excluding \n\0 
-     This twisted expression works up to n=227023 in 32-bit arithmetic
-     and for larger n if size_t has 64 bits.  */
-#define D6BODYLEN(n) \
-    ((n) * (size_t)((n) / 6) + (((n) * (size_t)((n) % 6) + 5) / 6))
-#define D6LEN(n) (1 + SIZELEN(n) + D6BODYLEN(n))
-/* exact digraph6 string length excluding \n\0 
-     This twisted expression works up to n=160529 in 32-bit arithmetic
-     and for larger n if size_t has 64 bits.  */
-
-#define TIMESWORDSIZE(w) ((w) << 5)
-#define SETWD(pos) ((pos) >> 5)
-#define SETBT(pos) ((pos)&037)
-#define ISELEMENT(setadd, pos) (((setadd)[SETWD(pos)] & bit[SETBT(pos)]) != 0)
-#define ADDELEMENT(setadd, pos) ((setadd)[SETWD(pos)] |= bit[SETBT(pos)])
-#define GRAPHROW(g, v, m) ((set *)(g) + (long)(v) * (long)(m))
-
-#define WORDSIZE 32
-
-#define GRAPH6_HEADER ">>graph6<<"
-#define SPARSE6_HEADER ">>sparse6<<"
-#define DIGRAPH6_HEADER ">>digraph6<<"
-
-#define GRAPH6 1
-#define SPARSE6 2
-#define DIGRAPH6 4
-#define UNKNOWN_TYPE 256
-#define HAS_HEADER 512
-
-#define EXTDEF_CLASS static
-#define EXTDEF_TYPE 2
-
-#if EXTDEF_TYPE == 1
-extern setword bit[];
-extern int bytecount[];
-extern int leftbit[];
-
-#else
-/* array giving setwords with single 1-bit */
-#if WORDSIZE == 64
-#ifdef SETWORD_LONGLONG
-EXTDEF_CLASS const
-    setword bit[] = {01000000000000000000000LL, 0400000000000000000000LL,
-                     0200000000000000000000LL, 0100000000000000000000LL,
-                     040000000000000000000LL, 020000000000000000000LL,
-                     010000000000000000000LL, 04000000000000000000LL,
-                     02000000000000000000LL, 01000000000000000000LL,
-                     0400000000000000000LL, 0200000000000000000LL,
-                     0100000000000000000LL, 040000000000000000LL,
-                     020000000000000000LL, 010000000000000000LL,
-                     04000000000000000LL, 02000000000000000LL,
-                     01000000000000000LL, 0400000000000000LL, 0200000000000000LL,
-                     0100000000000000LL, 040000000000000LL, 020000000000000LL,
-                     010000000000000LL, 04000000000000LL, 02000000000000LL,
-                     01000000000000LL, 0400000000000LL, 0200000000000LL,
-                     0100000000000LL, 040000000000LL, 020000000000LL, 010000000000LL,
-                     04000000000LL, 02000000000LL, 01000000000LL, 0400000000LL,
-                     0200000000LL, 0100000000LL, 040000000LL, 020000000LL,
-                     010000000LL, 04000000LL, 02000000LL, 01000000LL, 0400000LL,
-                     0200000LL, 0100000LL, 040000LL, 020000LL, 010000LL, 04000LL,
-                     02000LL, 01000LL, 0400LL, 0200LL, 0100LL, 040LL, 020LL, 010LL,
-                     04LL, 02LL, 01LL};
-#else
-EXTDEF_CLASS const
-    setword bit[] = {01000000000000000000000, 0400000000000000000000,
-                     0200000000000000000000, 0100000000000000000000,
-                     040000000000000000000, 020000000000000000000,
-                     010000000000000000000, 04000000000000000000,
-                     02000000000000000000, 01000000000000000000,
-                     0400000000000000000, 0200000000000000000,
-                     0100000000000000000, 040000000000000000, 020000000000000000,
-                     010000000000000000, 04000000000000000, 02000000000000000,
-                     01000000000000000, 0400000000000000, 0200000000000000,
-                     0100000000000000, 040000000000000, 020000000000000,
-                     010000000000000, 04000000000000, 02000000000000,
-                     01000000000000, 0400000000000, 0200000000000, 0100000000000,
-                     040000000000, 020000000000, 010000000000, 04000000000,
-                     02000000000, 01000000000, 0400000000, 0200000000, 0100000000,
-                     040000000, 020000000, 010000000, 04000000, 02000000, 01000000,
-                     0400000, 0200000, 0100000, 040000, 020000, 010000, 04000,
-                     02000, 01000, 0400, 0200, 0100, 040, 020, 010, 04, 02, 01};
-#endif
-#endif
-
-#if WORDSIZE == 32
-EXTDEF_CLASS const
-    setword bit[] = {020000000000, 010000000000, 04000000000, 02000000000,
-                     01000000000, 0400000000, 0200000000, 0100000000, 040000000,
-                     020000000, 010000000, 04000000, 02000000, 01000000, 0400000,
-                     0200000, 0100000, 040000, 020000, 010000, 04000, 02000, 01000,
-                     0400, 0200, 0100, 040, 020, 010, 04, 02, 01};
-#endif
-
-#if WORDSIZE == 16
-EXTDEF_CLASS const
-    setword bit[] = {0100000, 040000, 020000, 010000, 04000, 02000, 01000, 0400, 0200,
-                     0100, 040, 020, 010, 04, 02, 01};
-#endif
-
-/*  array giving number of 1-bits in bytes valued 0..255: */
-EXTDEF_CLASS const int bytecount[] = {0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,
-                                      1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
-                                      1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
-                                      2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
-                                      1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
-                                      2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
-                                      2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
-                                      3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
-                                      1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
-                                      2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
-                                      2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
-                                      3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
-                                      2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
-                                      3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
-                                      3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
-                                      4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8};
-
-/* array giving position (1..7) of high-order 1-bit in byte: */
-EXTDEF_CLASS const int leftbit[] = {8, 7, 6, 6, 5, 5, 5, 5, 4, 4, 4, 4, 4, 4, 4, 4,
-                                    3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-                                    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-                                    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-                                    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                                    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                                    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                                    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-#endif /* EXTDEFS */
-
-namespace cagesearch
+class CageSearcher
 {
-    typedef unsigned long setword;
-    typedef setword graph;
-
-    class CageSearcher
-    {
-    public:
-        CageSearcher();
-        graph *g;
-        int im;
-        int in;
-        void init_graph_from_str(std::string s);
-        void output_graph();
-        void output_graph(std::vector<std::vector<int>> &adjmat);
-    };
-    void stringtograph(std::string s, cagesearch::graph *g, int m);
-    int graphsize(std::string s);
-    static void gt_abort(char *msg);
-
-}
+public:
+    CageSearcher();
+    graph *g;
+    int im; // Size of store type, not meaningful for graph.
+    int in; // Size of graph, number of nodes.
+    void init_graph_from_str(std::string s);
+    void output_graph();
+    void output_graph(std::vector<std::vector<int>> &adjmat);
+    void output_color_pattern(int numaddons);
+    void output_color_pattern(int numaddons, std::vector<std::vector<int>> &colpair);
+};
+void stringtograph(std::string s, graph *g, int m);
+int graphsize(std::string s);
+static void gt_abort(char *msg);
+static int scan_console(int level, graph *g, _Boolean digraph, int *prev, long minedges, long maxedges,
+                        long sofar, long numcols, grouprec *group, int m, int n);
+static int trythisone2console(grouprec *group, graph *g, _Boolean digraph, int m, int n);
 
 #endif
--- src/cagesearch.cpp
+++ src/cagesearch.cpp
@@ -1,124 +1,294 @@
 // link to nauty library (Apache License)
 #include "cagesearch.h"
 
-namespace cagesearch
+// #define MAXARG 2000000000L
+// #define NOLIMIT (MAXARG + 31L)
+// #define TLS_ATTR
+// #define DYNALLSTAT(type, name, name_sz) \
+//     static TLS_ATTR type *name;         \
+//     static TLS_ATTR size_t name_sz = 0
+// DYNALLSTAT(int, id, id_sz);
+// DYNALLSTAT(int, allp, allp_sz);
+// DYNALLSTAT(set, workset, workset_sz);
+// DYNALLSTAT(int, workperm, workperm_sz);
+// DYNALLSTAT(int, bucket, bucket_sz);
+// DYNALLSTAT(set, dnwork, dnwork_sz);
+
+#define MAXNV 128
+static _Boolean first;
+static int col[MAXNV];
+static unsigned long groupsize;
+static unsigned long newgroupsize;
+static int lastreject[MAXNV];
+static _Boolean lastrejok;
+static int fail_level;
+// typedef unsigned long setword;
+// typedef setword graph;
+nauty_counter vc_nin, vc_nout;
+
+CageSearcher::CageSearcher()
 {
-    typedef unsigned long setword;
-    typedef setword graph;
+}
 
-    CageSearcher::CageSearcher()
+// convert graph6 format string (without header) to graph.
+void CageSearcher::init_graph_from_str(std::string s)
+{
+    int reqm = 0;
+    _Boolean digraph;
+    const char *p;
+    int m, n;
+    int readg_code;
+
+    if (s[0] == ':')
     {
+        digraph = FALSE;
+        p = s.data() + 1;
+    }
+    else if (s[0] == '&')
+    {
+        readg_code = DIGRAPH6;
+        digraph = TRUE;
+        p = s.data() + 1;
+        throw -1;
+        return;
+    }
+    else
+    {
+        readg_code = GRAPH6;
+        digraph = FALSE;
+        p = s.data();
     }
 
-    void CageSearcher::init_graph_from_str(std::string s)
+    while (toascii(*p) >= BIAS6 && toascii(*p) <= MAXBYTE)
     {
-        int reqm = 0;
-        _Boolean digraph;
-        const char *p;
-        int m, n;
-        int readg_code;
+        ++p;
+    }
+    if (*p == '\0')
+        gt_abort(">E readgg: missing newline\n");
+    else if (*p != '\n')
+    {
+        gt_abort(">E readgg: illegal character\n");
+    }
 
-        if (s[0] == ':')
+    n = graphsize(s);
+
+    if (readg_code == GRAPH6 && p - s.c_str() != G6LEN(n))
+        gt_abort(">E readgg: truncated graph6 line\n");
+    if (readg_code == DIGRAPH6 && p - s.c_str() != D6LEN(n))
+        gt_abort(">E readgg: truncated digraph6 line\n");
+
+    if (reqm > 0 && TIMESWORDSIZE(reqm) < n)
+        gt_abort(">E readgg: reqm too small\n");
+    else if (reqm > 0)
+        m = reqm;
+    else
+        m = (n + WORDSIZE - 1) / WORDSIZE;
+
+    if ((g = (graph *)calloc(n, m * sizeof(graph))) == NULL)
+        gt_abort(">E readgg: malloc failed\n");
+
+    in = n;
+    im = m;
+
+    stringtograph(s, g, m);
+}
+
+// print graph adjacent matrix to stdout.
+void CageSearcher::output_graph(void)
+{
+    set *gi;
+    int i, j;
+    int m = im;
+    int n = in;
+    _Boolean first;
+
+    for (i = 0, gi = (set *)g; i < n; ++i, gi += m)
+    {
+        first = TRUE;
+        for (j = 0; j < n; ++j)
         {
-            digraph = FALSE;
-            p = s.data() + 1;
+            if (!first)
+                std::cout << " ";
+            else
+                first = FALSE;
+            if (ISELEMENT(gi, j))
+                std::cout << "1";
+            else
+                std::cout << "0";
         }
-        else if (s[0] == '&')
+        std::cout << std::endl;
+    }
+}
+void CageSearcher::output_graph(std::vector<std::vector<int>> &adjmat)
+{
+    set *gi;
+    int i, j;
+    int m = im;
+    int n = in;
+
+    for (i = 0, gi = (set *)g; i < n; ++i, gi += m)
+    {
+        for (j = 0; j < n; ++j)
         {
-            readg_code = DIGRAPH6;
-            digraph = TRUE;
-            p = s.data() + 1;
+            if (ISELEMENT(gi, j))
+                adjmat[i][j] = 1;
+            else
+                adjmat[i][j] = 0;
+        }
+    }
+}
+
+void CageSearcher::output_color_pattern(int numaddons)
+{
+    static DEFAULTOPTIONS_GRAPH(options);
+    statsblk stats;
+    setword workspace[MAXNV];
+    grouprec *group;
+    int i, j, k, nloops;
+    set *gi, *gj;
+    int lab[MAXNV], ptn[MAXNV], orbits[MAXNV];
+    _Boolean loop[MAXNV];
+    int prev[MAXNV]; /* If >= 0, earlier point that must have greater colour */
+    int weight[MAXNV];
+    int region, start, stop;
+    int nfixed = 0;
+    int minedges, maxedges;
+
+    minedges = numaddons;
+    maxedges = numaddons;
+
+    int numcols = 2;
+
+    if (in > MAXNV)
+        gt_abort("Error: MAXNV exceeded\n");
+    // nauty_check(WORDSIZE, im, in, NAUTYVERSIONID);
+
+    nloops = 0;
+    for (i = 0, gi = g; i < in; ++i, gi += im)
+        if (ISELEMENT(gi, i))
+        {
+            DELELEMENT(gi, i);
+            loop[i] = TRUE;
+            ++nloops;
+        }
+        else
+            loop[i] = FALSE;
+
+    for (region = 0; region < 2; ++region)
+    {
+        if (region == 0)
+        {
+            if (nfixed == 0)
+                continue;
+            start = 0;
+            stop = nfixed;
+            if (stop > in)
+                stop = in;
         }
         else
         {
-            readg_code = GRAPH6;
-            digraph = FALSE;
-            p = s.data();
+            if (nfixed >= in)
+                continue;
+            start = nfixed;
+            stop = in;
         }
 
-        while (toascii(*p) >= BIAS6 && toascii(*p) <= MAXBYTE)
+        for (i = start, gi = g + im * (size_t)start; i < stop; ++i, gi += im)
         {
-            ++p;
-        }
-        if (*p == '\0')
-            gt_abort(">E readgg: missing newline\n");
-        else if (*p != '\n')
-        {
-            gt_abort(">E readgg: illegal character\n");
-        }
-
-        n = graphsize(s);
-
-        if (readg_code == GRAPH6 && p - s.c_str() != G6LEN(n))
-            gt_abort(">E readgg: truncated graph6 line\n");
-        if (readg_code == DIGRAPH6 && p - s.c_str() != D6LEN(n))
-            gt_abort(">E readgg: truncated digraph6 line\n");
-
-        if (reqm > 0 && TIMESWORDSIZE(reqm) < n)
-            gt_abort(">E readgg: reqm too small\n");
-        else if (reqm > 0)
-            m = reqm;
-        else
-            m = (n + WORDSIZE - 1) / WORDSIZE;
-
-        if ((g = (graph *)calloc(n, m * sizeof(graph))) == NULL)
-            gt_abort(">E readgg: malloc failed\n");
-
-        in = n;
-        im = m;
-
-        stringtograph(s, g, m);
-    }
-
-    void CageSearcher::output_graph(void)
-    {
-        set *gi;
-        int i, j;
-        int m = im;
-        int n = in;
-        _Boolean first;
-
-        for (i = 0, gi = (set *)g; i < n; ++i, gi += m)
-        {
-            first = TRUE;
-            for (j = 0; j < n; ++j)
+            /* Find most recent equivalent j. */
+            for (j = i - 1, gj = gi - im; j >= start; --j, gj -= im)
             {
-                if (!first)
-                    printf(" ");
-                else
-                    first = FALSE;
-                if (ISELEMENT(gi, j))
-                    printf("1");
-                else
-                    printf("0");
+                if (loop[j] != loop[i])
+                    continue;
+                for (k = 0; k < im; ++k)
+                    if (gi[k] != gj[k])
+                        break;
+                if (k < im)
+                {
+                    FLIPELEMENT(gi, i);
+                    FLIPELEMENT(gj, j);
+                    for (k = 0; k < im; ++k)
+                        if (gi[k] != gj[k])
+                            break;
+                    FLIPELEMENT(gi, i);
+                    FLIPELEMENT(gj, j);
+                }
+                if (k == im)
+                    break;
             }
-            printf("\n");
-        }
-    }
-    void CageSearcher::output_graph(std::vector<std::vector<int>> &adjmat)
-    {
-        set *gi;
-        int i, j;
-        int m = im;
-        int n = in;
-
-        _Boolean first;
-
-        for (i = 0, gi = (set *)g; i < n; ++i, gi += m)
-        {
-            for (j = 0; j < n; ++j)
+            if (j >= start)
             {
-                if (ISELEMENT(gi, j))
-                    adjmat[i][j] = 1;
-                else
-                    adjmat[i][j] = 0;
+                prev[i] = j;
+                weight[i] = weight[j] + 1;
+            }
+            else
+            {
+                prev[i] = -1;
+                weight[i] = 0;
             }
         }
     }
 
+    if (in == 0)
+    {
+        scan_console(0, g, FALSE, prev, minedges, maxedges, 0, numcols, FALSE, im, in);
+        return;
+    }
+
+    for (i = nfixed; i < in; ++i)
+        weight[i] += nfixed;
+
+    if (maxedges == NOLIMIT || maxedges > in * numcols)
+        maxedges = in * numcols;
+    if (in * numcols < minedges)
+        return;
+
+    options.userautomproc = groupautomproc;
+    options.userlevelproc = grouplevelproc;
+    options.defaultptn = FALSE;
+    options.digraph = (nloops > 0);
+
+    setlabptn(weight, lab, ptn, in);
+
+    if (nloops > 0)
+        for (i = 0, gi = g; i < in; ++i, gi += im)
+            if (loop[i])
+                ADDELEMENT(gi, i);
+
+    nauty(g, lab, ptn, NULL, orbits, &options, &stats, workspace, MAXNV, im, in, NULL);
+
+    if (stats.grpsize2 == 0)
+        groupsize = stats.grpsize1 + 0.1;
+    else
+        groupsize = 0;
+
+    group = groupptr(FALSE);
+    makecosetreps(group);
+
+    if (stats.numorbits < in)
+    {
+        j = in;
+        for (i = 0; i < in; ++i)
+            if (orbits[i] < i && orbits[i] < j)
+                j = orbits[i];
+
+        for (i = j + 1; i < in; ++i)
+            if (orbits[i] == j)
+                prev[i] = j;
+    }
+
+    lastrejok = FALSE;
+    for (i = 0; i < in; ++i)
+        col[i] = 0;
+
+    scan_console(0, g, FALSE, prev, minedges, maxedges, 0, numcols, group, im, in);
+}
+void CageSearcher::output_color_pattern(int numaddons, std::vector<std::vector<int>> &colpair)
+{
+}
 
 static void
-stringtograph(std::string s, cagesearch::graph *g, int m)
+stringtograph(std::string s, graph *g, int m)
 /* Convert string (graph6, digraph6 or sparse6 format) to graph. */
 /* Assumes g is big enough to hold it.   */
 {
@@ -295,11 +465,154 @@ int graphsize(std::string s)
     return n;
 }
 
+static int
+scan_console(int level, graph *g, _Boolean digraph, int *prev, long minedges, long maxedges,
+             long sofar, long numcols, grouprec *group, int m, int n)
+/* Recursive scan for default case */
+/* Returned value is level to return to. */
+{
+    int left;
+    long min, max, k, ret;
+
+    if (level == n)
+        return trythisone2console(group, g, digraph, m, n);
+
+    left = n - level - 1;
+    min = minedges - sofar - numcols * left;
+    if (min < 0)
+        min = 0;
+    max = maxedges - sofar;
+    if (max >= numcols)
+        max = numcols - 1;
+    if (prev[level] >= 0 && col[prev[level]] < max)
+        max = col[prev[level]];
+
+    for (k = min; k <= max; ++k)
+    {
+        col[level] = k;
+        ret = scan_console(level + 1, g, digraph, prev, minedges, maxedges, sofar + k, numcols, group, m, n);
+        if (ret < level)
+            return ret;
+    }
+
+    return level - 1;
+}
+
+static int
+ismax(int *p, int n)
+/* test if col^p <= col */
+{
+    int i, k;
+    int fail;
+
+    fail = 0;
+    for (i = 0; i < n; ++i)
+    {
+        k = p[i];
+        if (k > fail)
+            fail = k;
+        if (col[k] > col[i])
+        {
+            fail_level = fail;
+            return FALSE;
+        }
+        else if (col[k] < col[i])
+            return TRUE;
+    }
+
+    ++newgroupsize;
+    return TRUE;
+}
+
+/**************************************************************************/
+
+static void
+testmax(int *p, int n, int *abort)
+/* Called by allgroup2. */
+{
+    int i;
+
+    if (first)
+    { /* only the identity */
+        first = FALSE;
+        return;
+    }
+
+    if (!ismax(p, n))
+    {
+        *abort = 1;
+        for (i = 0; i < n; ++i)
+            lastreject[i] = p[i];
+        lastrejok = TRUE;
+    }
+}
+
+static int
+trythisone2console(grouprec *group, graph *g, _Boolean digraph, int m, int n)
+/* Try one solution, accept if maximal. */
+/* Return value is level to return to. */
+{
+    int i, j;
+    _Boolean accept;
+    graph *gi;
+    size_t ne;
+
+    newgroupsize = 1;
+
+    if (!group || groupsize == 1)
+        accept = TRUE;
+    else if (lastrejok && !ismax(lastreject, n))
+        accept = FALSE;
+    else if (lastrejok && groupsize == 2)
+        accept = TRUE;
+    else
+    {
+        newgroupsize = 1;
+        first = TRUE;
+
+        if (allgroup2(group, testmax) == 0)
+            accept = TRUE;
+        else
+            accept = FALSE;
+    }
+
+    if (accept)
+    {
+        ++vc_nout;
+
+        ne = 0;
+        for (gi = g + m * (size_t)n; --gi >= g;)
+            ne += POPCOUNT(*gi);
+        if (!digraph)
+        {
+            for (i = 0, gi = g; i < n; ++i, gi += m)
+                if (ISELEMENT(gi, i))
+                    ++ne;
+            ne /= 2;
+        }
+        // fprintf(outfile, "%d %lu", n, (unsigned long)ne);
+
+        for (i = 0; i < n; ++i)
+            if(col[i])
+            std::cout << i<<" ";
+        std::cout << " ";
+        // for (i = 0, gi = g; i < n; ++i, gi += m)
+        // {
+        //     for (j = (digraph ? -1 : i - 1); (j = nextelement(gi, m, j)) >= 0;)
+        //         fprintf(outfile, " %d %d", i, j);
+        // }
+        std::cout << "\n";
+
+        return n - 1;
+    }
+    else
+        return fail_level - 1;
+}
+
 static void
 gt_abort(char *msg) /* Write message and halt. */
 {
     if (msg)
         std::cerr << msg;
     exit(1);
-}
 }
