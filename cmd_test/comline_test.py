e1d8461f9d1aa28d3a790a8263bac3957b84f7bf
Franklalalala <1660810667@qq.com>
2021-12-26 14:04:47 +0800

    Refactored commandline feature, add random and step mode.

---
+++ cmd_test/comline_test.py
@@ -0,0 +1,46 @@
+import networkx as nx
+from fullerenedatapraser.molecular.fullerene import FullereneFamily
+from fullerenedatapraser.io.xyz import simple_read_xyz_xtb
+from fullerenedatapraser.io.recursion import recursion_files
+import os
+
+
+SOURCE_ROOT = r'./'
+path_cage = r'./'
+addon_num = 4
+
+CAGE_SEARCH_PATH = r'../bin/cagesearch.exe'
+
+
+def get_one_atoms(source_root_path):
+    for item in recursion_files(source_root_path, ignore_mode=True):
+        s_atoms = list(simple_read_xyz_xtb(item, read_comment=False))[0]
+        yield {"atoms": s_atoms, "name": os.path.splitext(os.path.basename(item))[0]}
+
+
+for result in get_one_atoms(source_root_path=SOURCE_ROOT):
+    s_atoms = result["atoms"]
+    filename = result["name"]
+    f = FullereneFamily(spiral=0, atoms=s_atoms)
+    graph6str = nx.to_graph6_bytes(f.graph, header=False).decode().split()[0]
+
+    mode = 'random'
+    path_out = os.path.join(path_cage, f'{mode}_{filename}_{addon_num}addons.out')
+    os.system(f"{CAGE_SEARCH_PATH} --graph6str \"{graph6str}\" --addnum \"{addon_num}\" -o \"{path_out}\" -m \"{mode}\" -r 100")
+
+    mode = 'base'
+    path_out = os.path.join(path_cage, f'{mode}_{filename}_{addon_num}addons.out')
+    os.system(f"{CAGE_SEARCH_PATH} --graph6str \"{graph6str}\" --addnum \"{addon_num}\" -o \"{path_out}\" -m \"{mode}\"")
+
+    mode = 'step'
+    path_out = os.path.join(path_cage, f'{mode}_normal_{filename}_{addon_num}addons.out')
+    os.system(f"{CAGE_SEARCH_PATH} --graph6str \"{graph6str}\" --addnum \"{addon_num}\" -o \"{path_out}\" -m \"{mode}\" 17 12") # Normal
+    addon_num = 6  # There are no patterns in addon_num=4, for 1812_C60, 59 58
+    path_out = os.path.join(path_cage, f'{mode}_caution_abnormal_{filename}_{addon_num}addons.out')
+    os.system(f"{CAGE_SEARCH_PATH} --graph6str \"{graph6str}\" --addnum  \"{addon_num}\"  -o \"{path_out}\" -m \"{mode}\" 59 58") # The situation of old color[i] > n-3, the completeness is ensured.
+
+    mode = 'console'
+    os.system(f"{CAGE_SEARCH_PATH} --graph6str \"{graph6str}\" --addnum \"{addon_num}\" -m \"{mode}\"")
+
+    mode = 'stdvec'
+    os.system(f"{CAGE_SEARCH_PATH} --graph6str \"{graph6str}\" --addnum \"{addon_num}\" -m \"{mode}\"")
--- include/cagesearch.h
+++ include/cagesearch.h
@@ -5,15 +5,17 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
 #include <ctime>
 #include <fstream>
 #include <strstream>
 #include <iomanip>
-
 #include "nauty.h"
 #include "gtools.h"
 #include "naugroup.h"
+#include <algorithm>
+#include <random>
+#include <unordered_set>
+
 
 // #include "graph6.hpp"
 #include "cmdline.h"
@@ -25,24 +27,34 @@ class CageSearcher
 {
 public:
     CageSearcher();
-    graph *g;
+    graph* g;
     int im; // Size of store type, not meaningful for graph.
     int in; // Size of graph, number of nodes.
+    grouprec* group;  //cage's group, type of nautygroup
+
+
     void init_graph_from_str(std::string s);
     void output_graph();
-    void output_graph(std::vector<std::vector<int>> &adjmat);
+    void initialize_scan(int numaddons);
+    void output_graph(std::vector<std::vector<int>>& adjmat);
     void output_color_pattern(int numaddons);
-    void output_color_pattern(int numaddons, std::vector<std::vector<int>> &colpair);
-    void output_color_pattern(int numaddons, std::ofstream &output_color_pattern_to_file);
+    void output_color_pattern(int numaddons, std::vector<std::vector<int>>& colpair);
+    void output_color_pattern(int numaddons, std::ofstream& output_color_pattern_to_file);
+    void output_color_pattern(int numaddons, std::ofstream& output_color_pattern_to_file, int random_num);
+    void output_color_pattern(int numaddons, std::ofstream& output_color_pattern_to_file, std::vector<int>& old_color);
 };
-void stringtograph(std::string s, graph *g, int m);
+void stringtograph(std::string s, graph* g, int m);
 int graphsize(std::string s);
-static void gt_abort(char *msg);
-static int scan_console(int level, graph *g, _Boolean digraph, int *prev, long minedges, long maxedges, long sofar, long numcols, grouprec *group, int m, int n);
-static int trythisone2console(grouprec *group, graph *g, _Boolean digraph, int m, int n);
-static int scan_stdvec(int level, graph *g, _Boolean digraph, int *prev, long minedges, long maxedges, long sofar, long numcols, grouprec *group, int m, int n, std::vector<std::vector<int>> &colpair);
-static int trythisone2stdvec(grouprec *group, graph *g, _Boolean digraph, int m, int n, std::vector<std::vector<int>> &colpair);
-static int scan_fstream(int level, graph *g, _Boolean digraph, int *prev, long minedges, long maxedges, long sofar, long numcols, grouprec *group, int m, int n, std::ofstream &output_color_pattern_to_file);
-static int trythisone2fstream(grouprec *group, graph *g, _Boolean digraph, int m, int n, std::ofstream &output_color_pattern_to_file);
+static void gt_abort(char* msg);
+static int scan_console(int level, graph* g, _Boolean digraph, int* prev, long minedges, long maxedges, long sofar, long numcols, grouprec* group, int m, int n);
+static int trythisone2console(grouprec* group, graph* g, _Boolean digraph, int m, int n);
+static int scan_stdvec(int level, graph* g, _Boolean digraph, int* prev, long minedges, long maxedges, long sofar, long numcols, grouprec* group, int m, int n, std::vector<std::vector<int>>& colpair);
+static int trythisone2stdvec(grouprec* group, graph* g, _Boolean digraph, int m, int n, std::vector<std::vector<int>>& colpair);
 
+static int scan_fstream(int level, graph* g, _Boolean digraph, int* prev, long minedges, long maxedges, long sofar, long numcols, grouprec* group, int m, int n, std::ofstream& output_color_pattern_to_file);
+
+static int trythisone2fstream(grouprec* group, graph* g, _Boolean digraph, int m, int n, std::ofstream& output_color_pattern_to_file);
+static int trythisone2fstream_random(grouprec* group, graph* g, _Boolean digraph, int m, int n, std::ofstream& output_color_pattern_to_file, int output_monitor, std::unordered_set<std::string>& ptn_uset);
+static int scan_fstream_stepwise(int level, graph* g, _Boolean digraph, int* prev, long minedges, long maxedges, long sofar, long numcols, grouprec* group, int m, int n, std::ofstream& output_color_pattern_to_file, std::vector<int>& old_color, int numaddons);
+void scan_fstream_random(graph* g, _Boolean digraph, grouprec* group, int m, int n, std::ofstream& output_color_pattern_to_file, int random_num, int numaddons);
 #endif
